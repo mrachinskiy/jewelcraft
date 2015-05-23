cf7fb66db2db68215461f5b1cbeb84f0ca494a1b
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2015-05-23 18:29:14 +0400

    Major code refactor and fix couple issues

--- helpers.py
+++ helpers.py
@@ -31,19 +31,22 @@ from . import volume
 
 
 
+
+
 def ShowErrorMessage(message, wrap=80):
 	lines = []
+	append = lines.append
 	if wrap > 0:
 		while len(message) > wrap:
 			i = message.rfind(' ',0,wrap)
 			if i == -1:
-				lines.append(message[:wrap])
+				append(message[:wrap])
 				message = message[wrap:]
 			else:
-				lines.append(message[:i])
+				append(message[:i])
 				message = message[i+1:]
 	if message:
-		lines.append(message)
+		append(message)
 	def draw(self, context):
 		for line in lines:
 			self.layout.label(line)
@@ -52,19 +55,21 @@ def ShowErrorMessage(message, wrap=80):
 
 
 
+
+
 class Utils():
 
 
 	def make_dupliface(self):
-		D = bpy.data
-		C = bpy.context
+		context = bpy.context
+		data = bpy.data
 		convert = Convert()
 
 		gem = False
 		name = ''
 		obs = []
 		append = obs.append
-		for ob in C.selected_objects:
+		for ob in context.selected_objects:
 			if (ob.data and ob.data.get('gem')):
 				gem = ob
 				append(ob)
@@ -78,17 +83,23 @@ class Utils():
 			ob = obs[0]
 		
 
-		verts = [(-0.15, 0.15, 0), (0.15, 0.15, 0), (0.15, -0.15, 0), (-0.15, -0.15, 0)]
-		faces = [(3, 2, 1, 0)]
+		verts = [(-0.15,0.15,0), (0.15,0.15,0), (0.15,-0.15,0), (-0.15,-0.15,0)]
+		faces = [(3,2,1,0)]
 
-		me = D.meshes.new(name+'Duplifaces')
+
+		offset = (ob.dimensions[0]+1, 0, 0)
+		for i in range(len(verts)):
+			verts[i] = tuple(x+y for x,y in zip(verts[i], offset))
+
+
+		me = data.meshes.new(name+'Duplifaces')
 		me.from_pydata(verts, [], faces)
 		me.update()
 
-		df = D.objects.new(name+'Duplifaces', me)
+		df = data.objects.new(name+'Duplifaces', me)
 		df.location = ob.location
 		df.dupli_type = 'FACES'
-		C.scene.objects.link(df)
+		context.scene.objects.link(df)
 
 		for ob in obs:
 			ob.parent = df
@@ -106,7 +117,7 @@ class Utils():
 						v_co.append(v.co)
 
 		if v_co:
-			size = round(Vector(x * y for x, y in zip(v_co[0]-v_co[1], ob.scale)).length, 2)
+			size = round(Vector(x*y for x,y in zip(v_co[0]-v_co[1], ob.scale)).length, 2)
 		else:
 			size = False
 
@@ -117,10 +128,10 @@ class Utils():
 		props = bpy.context.scene.jewelcraft
 		dens = props.crystal_density[tpe]
 		corr = props.gems_volume_correction[cut]
-		
+
 		if d2 == False:
 			d2 = d
-		
+
 		if h == False:
 			if d <= d2:
 				h = d * 0.606
@@ -160,6 +171,8 @@ class Utils():
 
 
 
+
+
 class Check():
 
 
@@ -180,6 +193,8 @@ class Check():
 
 
 
+
+
 class Convert():
 
 
@@ -187,25 +202,33 @@ class Convert():
 		return float(value.replace(',', '.'))
 
 
-	def to_size(self, size, ob, obj=False, tolength=False):
+	def to_length(self, ob, obj):
 		utils = Utils()
-		
-		if obj:
-			for vg in obj.vertex_groups:
-				if vg.name == 'Size 1':
-					size = utils.get_size(obj, vg.index)
 
-		if not tolength:
-			if size <= 0:
-				size = 1
-			ob.dimensions = ob.dimensions * size
+		for vg in obj.vertex_groups:
+			if vg.name == 'Size 1':
+				size = utils.get_size(obj, vg.index)
 
-		else:
-			size2 = size
-			for vg in obj.vertex_groups:
-				if vg.name == 'Size 2':
-					size2 = utils.get_size(obj, vg.index)
-			ob.dimensions = [ob.dimensions[0] * size, size2, ob.dimensions[2] * size]
+		size2 = size
+		for vg in obj.vertex_groups:
+			if vg.name == 'Size 2':
+				size2 = utils.get_size(obj, vg.index)
+
+		ob.dimensions = [ob.dimensions[0]*size, size2, ob.dimensions[2]*size]
+
+
+	def to_width(self, ob, obj):
+		for vg in obj.vertex_groups:
+			if vg.name == 'Size 1':
+				size = Utils().get_size(obj, vg.index)
+		self.to_size(ob, size)
+
+
+	def to_size(self, ob, size):
+		if size <= 0:
+			size = 1
+		ob.dimensions = ob.dimensions * size
+
 
 	def apply_transforms(self, ob):
 		if ob.type == 'MESH':
@@ -225,73 +248,70 @@ class Convert():
 
 
 
+
+
 class Asset():
 
 
-	def __init__(self):
-		self.D = bpy.data
-		self.C = bpy.context
-		self.sce = self.C.scene
-
-
-	def gem(self, data_to, cut, tpe, size, obj=False):
-		sce = self.sce
+	def gem_import(self, data_to, cut, tpe, size):
 		ob = data_to.objects[0]
-		convert = Convert()
-		ob_type = 'GEM'
+		ob.data['gem'] = {}
+		ob.location = bpy.context.scene.cursor_location
+		Convert().to_size(ob, size)
 
-		ob.data['gem'] = {'CUT': cut}
+		self.type_set(ob, tpe, data_to.materials)
+		self.gem_sce_link(ob, cut)
 
-		if not obj:
+
+	def gem_replace(self, data_to, cut, tpe, obj):
+		ob = data_to.objects[0]
+		ob.data['gem'] = {}
+		if obj.parent:
 			ob.location = [0,0,0]
-			convert.to_size(size, ob)
-			self.material(ob_type, ob, data_to.materials, tpe)
-
+			ob.parent = obj.parent
 		else:
-			if obj.parent:
-				ob.location = [0,0,0]
-				ob.parent = obj.parent
-			else:
-				ob.location = obj.location
+			ob.location = obj.location
+		ob.rotation_euler = obj.rotation_euler
 
-			convert.to_size(size, ob, obj)
-			self.material(ob_type, ob, [obj.material_slots[0].material], tpe)
+		Convert().to_width(ob, obj)
+		self.type_copy(ob, obj)
 
-			sce.objects.unlink(obj)
-			self.D.objects.remove(obj)
+		bpy.context.scene.objects.unlink(obj)
+		bpy.data.objects.remove(obj)
+		self.gem_sce_link(ob, cut)
 
+
+	def gem_sce_link(self, ob, cut):
+		sce = bpy.context.scene
+
+		ob.data['gem']['CUT'] = cut
 		sce.objects.link(ob)
 		sce.objects.active = ob
 		ob.select = True
-		convert.apply_transforms(ob)
+		Convert().apply_transforms(ob)
 
 
-	def material(self, ob_type, ob, materials, tpe=False):
-		D = self.D
+	def type_set(self, ob, tpe, materials):
+		ob.data['gem']['TYPE'] = tpe
+		self.material_assign(ob, tpe.title(), materials)
 
 
-		def get_material(name):
-			material = False
-			for mat in materials:
-				if mat.name == name:
-					material = mat
-			if not material:
-				material = D.materials[name]
-			return material
+	def type_copy(self, ob, obj):
+		ob.data['gem']['TYPE'] = obj.data['gem']['TYPE']
+		append = ob.data.materials.append
+		for mat in obj.data.materials:
+			append(mat)
 
 
-		if ob_type == 'GEM':
-			ob.data['gem']['TYPE'] = tpe
-			material = get_material(tpe.title())
+	def material_assign(self, ob, name, materials):
+		material = False
+		for mat in materials:
+			if mat.name == name:
+				material = mat
+		if not material:
+			material = bpy.data.materials[name]
 
-		elif ob_type == 'PRONGS':
-			material = get_material('Prongs')
-
-		elif ob_type == 'CUTTER':
-			material = get_material('Cutter')
-
-
-		if self.sce.render.engine == 'BLENDER_RENDER':
+		if bpy.context.scene.render.engine == 'BLENDER_RENDER':
 			material.use_nodes = False
 
 		if len(ob.material_slots) < 1:
@@ -300,74 +320,72 @@ class Asset():
 			ob.material_slots[0].material = material
 
 
-	def prongs(self, data_to, size, obj=False):
-		convert = Convert()
-		ob_type = 'PRONGS'
-		
-		if not obj:
-			for iob in data_to.objects:
-				if 'Prongs' in iob.name:
-					ob = iob
-					ob.location = [0,0,0]
-					ob.dimensions = ob.dimensions * size
+	def prongs_import(self, data_to, size):
+		for ob in data_to.objects:
+			if 'Prongs' in ob.name:
+				ob.location = bpy.context.scene.cursor_location
+				ob.dimensions = ob.dimensions * size
+				self.prongs_sce_link(ob, data_to)
 
+
+	def prongs_append(self, data_to, obj):
+		ob = data_to.objects[0]
+		if obj.parent:
+			ob.location = [0,0,0]
+			ob.parent = obj.parent
 		else:
-			ob = data_to.objects[0]
-			if obj.parent:
-				ob.location = [0,0,0]
-				ob.parent = obj.parent
-			else:
-				ob.location = obj.location
-			convert.to_size(size, ob, obj)
-		
-		self.material(ob_type, ob, data_to.materials)
-		self.sce.objects.link(ob)
+			ob.location = obj.location
+		ob.rotation_euler = obj.rotation_euler
+		Convert().to_width(ob, obj)
+		self.prongs_sce_link(ob, data_to)
+
+
+	def prongs_sce_link(self, ob, data_to):
+		self.material_assign(ob, 'Prongs', data_to.materials)
+		bpy.context.scene.objects.link(ob)
 		ob.select = True
-		convert.apply_transforms(ob)
+		Convert().apply_transforms(ob)
 
 
-	def cutter(self, data_to, size, obj=False):
-		convert = Convert()
-		ob_type = 'CUTTER'
-		
-		if not obj:
-			for iob in data_to.objects:
-				if 'Cutter' in iob.name:
-					ob = iob
-					ob.location = [0,0,0]
-					ob.dimensions = ob.dimensions * size
+	def cutter_import(self, data_to, size):
+		for ob in data_to.objects:
+			if 'Cutter' in ob.name:
+				ob.location = bpy.context.scene.cursor_location
+				ob.dimensions = ob.dimensions * size
+				self.cutter_sce_link(ob, data_to)
 
+
+	def cutter_append(self, data_to, obj):
+		ob = data_to.objects[0]
+		if obj.parent:
+			ob.location = [0,0,0]
+			ob.parent = obj.parent
 		else:
-			ob = data_to.objects[0]
-			if obj.parent:
-				ob.location = [0,0,0]
-				ob.parent = obj.parent
-			else:
-				ob.location = obj.location
-			convert.to_size(size, ob, obj, True)
+			ob.location = obj.location
+		ob.rotation_euler = obj.rotation_euler
+		Convert().to_length(ob, obj)
+		self.cutter_sce_link(ob, data_to)
 
-		self.material(ob_type, ob, data_to.materials)
-		self.sce.objects.link(ob)
+
+	def cutter_sce_link(self, ob, data_to):
+		self.material_assign(ob, 'Cutter', data_to.materials)
+		bpy.context.scene.objects.link(ob)
 		ob.select = True
-		convert.apply_transforms(ob)
+		Convert().apply_transforms(ob)
+
+
 
 
 
 
 class Import():
-
-
-	def __init__(self):
-		self.D = bpy.data
-		self.C = bpy.context
-		self.props = self.C.scene.jewelcraft
-		self.l = localization.locale[self.props.lang]
-		self.filepath = path.join(path.dirname(__file__), 'assets', 'gems.blend')
+	filepath = path.join(path.dirname(__file__), 'assets', 'gems.blend')
 
 
 	def gem(self):
-		D = self.D
-		props = self.props
+		data = bpy.data
+		props = bpy.context.scene.jewelcraft
+		l = localization.locale[props.lang]
 		tpe = props.import_gem_type
 		cut = props.import_gem_cut
 		size = Check().size(cut)
@@ -376,9 +394,9 @@ class Import():
 		asset = Asset()
 
 		if not size:
-			return ShowErrorMessage(self.l['error_digit'])
+			return ShowErrorMessage(l['error_digit'])
 
-		with D.libraries.load(self.filepath) as (data_from, data_to):
+		with data.libraries.load(self.filepath) as (data_from, data_to):
 			
 			data_to.objects = [cut.title()]
 			if prongs:
@@ -386,96 +404,90 @@ class Import():
 			if cutter:
 				data_to.objects.append(cut.title()+' Cutter')
 
-			if tpe.title() not in D.materials:
+			if tpe.title() not in data.materials:
 				data_to.materials.append(tpe.title())
-			if 'Prongs' not in D.materials:
+			if 'Prongs' not in data.materials:
 				data_to.materials.append('Prongs')
-			if 'Cutter' not in D.materials:
+			if 'Cutter' not in data.materials:
 				data_to.materials.append('Cutter')
 
 		bpy.ops.object.select_all(action='DESELECT')
 
-		asset.gem(data_to, cut, tpe, size)
+		asset.gem_import(data_to, cut, tpe, size)
 		if prongs:
-			asset.prongs(data_to, size)
+			asset.prongs_import(data_to, size)
 		if cutter:
-			asset.cutter(data_to, size)
-		
+			asset.cutter_import(data_to, size)
 
 
 	def type(self):
-		D = self.D
-		props = self.props
-		tpe = props.import_gem_type
-		obs = self.C.selected_objects
+		context = bpy.context
+		data = bpy.data
+		tpe = context.scene.jewelcraft.import_gem_type
 		asset = Asset()
-		ob_type = 'GEM'
 
-		with D.libraries.load(self.filepath) as (data_from, data_to):
-			if tpe.title() not in D.materials:
+		with data.libraries.load(self.filepath) as (data_from, data_to):
+			if tpe.title() not in data.materials:
 				data_to.materials = [tpe.title()]
 
-		for ob in obs:
+		for ob in context.selected_objects:
 			if ob.data.get('gem'):
-				asset.material(ob_type, ob, data_to.materials, tpe)
+				asset.type_set(ob, tpe, data_to.materials)
 
 
 	def cut(self):
-		D = self.D
-		props = self.props
+		context = bpy.context
+		data = bpy.data
+		props = context.scene.jewelcraft
 		tpe = props.import_gem_type
 		cut = props.import_gem_cut
-		obs = self.C.selected_objects
 		asset = Asset()
-		size = None
 
-		for ob in obs:
+		for ob in context.selected_objects:
 			if ob.data.get('gem'):
 
-				with D.libraries.load(self.filepath) as (data_from, data_to):
+				with data.libraries.load(self.filepath) as (data_from, data_to):
 					data_to.objects = [cut.title()]
 
-				asset.gem(data_to, cut, tpe, size, ob)
+				asset.gem_replace(data_to, cut, tpe, ob)
 
 
 	def prongs(self):
-		D = self.D
-		obs = self.C.selected_objects
+		data = bpy.data
 		asset = Asset()
-		size = None
 
-		for ob in obs:
+		for ob in bpy.context.selected_objects:
 			if ob.data.get('gem'):
 				cut = ob.data['gem']['CUT']
 
-				with D.libraries.load(self.filepath) as (data_from, data_to):
+				with data.libraries.load(self.filepath) as (data_from, data_to):
 
 					data_to.objects = [cut.title()+' Prongs']
 
-					if 'Prongs' not in D.materials:
+					if 'Prongs' not in data.materials:
 						data_to.materials.append('Prongs')
 
-				asset.prongs(data_to, size, ob)
+				asset.prongs_append(data_to, ob)
 
 
 	def cutter(self):
-		D = self.D
-		obs = self.C.selected_objects
+		data = bpy.data
 		asset = Asset()
-		size = None
 
-		for ob in obs:
+		for ob in bpy.context.selected_objects:
 			if ob.data.get('gem'):
 				cut = ob.data['gem']['CUT']
 
-				with D.libraries.load(self.filepath) as (data_from, data_to):
+				with data.libraries.load(self.filepath) as (data_from, data_to):
 
 					data_to.objects = [cut.title()+' Cutter']
 
-					if 'Cutter' not in D.materials:
+					if 'Cutter' not in data.materials:
 						data_to.materials.append('Cutter')
 
-				asset.cutter(data_to, size, ob)
+				asset.cutter_append(data_to, ob)
+
+
 
 
 
@@ -483,13 +495,8 @@ class Import():
 class Stats():
 
 
-	def __init__(self):
-		self.C = bpy.context
-		self.sce = self.C.scene
-
-
 	def get(self):
-		sce = self.sce
+		sce = bpy.context.scene
 		obs = sce.objects
 		props = sce.jewelcraft
 		stats = {}
@@ -536,7 +543,7 @@ class Stats():
 			mo = ob.modifiers[mos[-1]]
 			save_state = mo.show_viewport
 			mo.show_viewport = False
-			self.sce.update()
+			bpy.context.scene.update()
 			stats = [str(round(ob.dimensions[1], 1)),
 			         str(round(ob.dimensions[2], 1))]
 			mo.show_viewport = save_state
@@ -548,12 +555,10 @@ class Stats():
 
 
 	def gems(self):
-		sce = self.sce
-		obs = sce.objects
 		utils = Utils()
 		stats = {}
 		
-		for ob in sce.objects:
+		for ob in bpy.context.scene.objects:
 			
 			if (ob.type == 'MESH' and ob.data.get('gem') and ob.vertex_groups):
 
@@ -614,16 +619,13 @@ class Stats():
 
 
 
+
+
 class Template():
 
 
-	def __init__(self):
-		self.C = bpy.context
-		self.l = localization.locale[bpy.context.scene.jewelcraft.lang]
-
-
 	def make(self, stats):
-		l = self.l
+		l = localization.locale[bpy.context.scene.jewelcraft.lang]
 		t = ''
 		mm = l['mm']
 
@@ -646,42 +648,34 @@ class Template():
 			t += '\n'
 
 		if stats['GEMS']:
-			table_columns = '{:9} | {:8} | {:25} | {}\n    '
-			
-			t += l['t_settings'] + '\n    '
-			t += table_columns.format(l['type'], l['cut'], l['size'], l['qty'])
-			t += '—' * 70 + '\n    '
+			col_len = [len(l['type']), len(l['cut']), len(l['size']), len(l['qty'])]
+			rows = []
+			append = rows.append
 			for tpe in sorted(stats['GEMS']):
 				for cut in sorted(stats['GEMS'][tpe]):
 					for size in sorted(stats['GEMS'][tpe][cut]):
-						t += self.gems(stats['GEMS'][tpe][cut][size], tpe, cut, size, table_columns)
+						row = self.gems(tpe, cut, size, stats['GEMS'][tpe][cut][size])
+						append(row)
+						for i in range(len(col_len)):
+							if len(row[i]) > col_len[i]:
+								col_len[i] = len(row[i])
+
+			table_columns = '{:'+str(col_len[0])+'} | {:'+str(col_len[1])+'} | {:'+str(col_len[2])+'} | {}\n    '
+			underline_len = col_len[0]+col_len[1]+col_len[2]+col_len[3]+10
+
+			t += l['t_settings']+'\n    '
+			t += table_columns.format(l['type'], l['cut'], l['size'], l['qty'])
+			t += '—'*underline_len+'\n    '
+			for gem in rows:
+				t += table_columns.format(gem[0], gem[1], gem[2], gem[3])
+
 		return t
 
 
-	def weight(self, vol, metal):
-		props = self.C.scene.jewelcraft
-		l = self.l
-		g = l['g']
-
-		if metal == 'CUSTOM':
-			if (props.metal_custom_density and Check().digit(props.metal_custom_density)):
-				custom_density = Convert().to_float(props.metal_custom_density) / 1000
-				
-				result = str(round(vol * custom_density, 2)) + g + ' ('+props.metal_custom_name+')'
-			else:
-				result = 0
-	
-		else:
-			dens = props.metal_density[metal]
-
-			result = str(round(vol * dens, 2)) + g + ' ('+l[metal.lower()]+')'
-
-		return result
-
-
-	def gems(self, qty, tpe, cut, size, table_columns):
-		dt = self.C.scene.jewelcraft.diamonds_table
-		l = self.l
+	def gems(self, tpe, cut, size, qty):
+		props = bpy.context.scene.jewelcraft
+		l = localization.locale[props.lang]
+		dt = props.diamonds_table
 		mm = l['mm']
 		ct = l['ct']
 		itms = l['items']
@@ -705,7 +699,30 @@ class Template():
 		Type = l[tpe.lower()]
 		Cut = l[cut.lower()]
 
-		return table_columns.format(Type, Cut, Size, Qty)
+		return (Type, Cut, Size, Qty)
+
+
+	def weight(self, vol, metal):
+		props = bpy.context.scene.jewelcraft
+		l = localization.locale[props.lang]
+		g = l['g']
+
+		if metal == 'CUSTOM':
+			if (props.metal_custom_density and Check().digit(props.metal_custom_density)):
+				custom_density = Convert().to_float(props.metal_custom_density) / 1000
+
+				result = str(round(vol * custom_density, 2)) + g + ' ('+props.metal_custom_name+')'
+			else:
+				result = 0
+
+		else:
+			dens = props.metal_density[metal]
+
+			result = str(round(vol * dens, 2)) + g + ' ('+l[metal.lower()]+')'
+
+		return result
+
+
 
 
 
@@ -713,32 +730,24 @@ class Template():
 class Export():
 
 
-	def __init__(self):
-		self.C = bpy.context
-		self.l = localization.locale[self.C.scene.jewelcraft.lang]
-
-		self.export_stats()
-
-
-	def export_stats(self):
-		t = Template().make(Stats().get())
-
-		self.save(t)
-
-
-	def save(self, t):
+	def stats(self):
 		filepath = bpy.data.filepath
+		l = localization.locale[bpy.context.scene.jewelcraft.lang]
 		
 		if filepath:
+			t = Template().make(Stats().get())
+
 			if filepath.rfind('\\') != -1:
 				last_slash = filepath.rfind('\\')
 			else:
 				last_slash = filepath.rfind('/')
-			
-			save_path = path.join(filepath[:last_slash], 'stats.txt')
+
+			filename = bpy.path.display_name_from_filepath(filepath)
+			save_path = path.join(filepath[:last_slash], filename+'_stats.txt')
 			
 			f = open(save_path, 'w', encoding='utf-8')
 			f.write(t)
 			f.close()
+
 		else:
-			return ShowErrorMessage(self.l['error_file'])
+			return ShowErrorMessage(l['error_file'])
