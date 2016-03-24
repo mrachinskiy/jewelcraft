853764cdcc7afbfbad4c425ec52f73987607eec1
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2016-03-25 02:17:58 +0400

    Cleanup: style changes and other little things

--- modules/utility.py
+++ modules/utility.py
@@ -9,11 +9,13 @@ def show_error_message(message):
 	bpy.context.window_manager.popup_menu(draw, title="Error", icon="ERROR")
 
 
-# Forward compatibility function, should be removed somewhere in 2.x release
 def ob_prop_style_convert(ob):
-	if ob.data['gem'].get('TYPE'):
+	# Forward compatibility function, should be removed somewhere in 2.x release
+
+	if 'TYPE' in ob.data['gem']:
 		ob.data['gem']['type'] = ob.data['gem']['TYPE']
 		del ob.data['gem']['TYPE']
-	if ob.data['gem'].get('CUT'):
+
+	if 'CUT' in ob.data['gem']:
 		ob.data['gem']['cut'] = ob.data['gem']['CUT']
 		del ob.data['gem']['CUT']
