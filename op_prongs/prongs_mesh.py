88592bddbcf5e7426aeb3d5c61c99c7f87fc4546
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2019-03-31 03:46:03 +0400

    Add Prongs: better defaults for extremely small gems

--- op_prongs/prongs_mesh.py
+++ op_prongs/prongs_mesh.py
@@ -19,7 +19,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 
-from math import sin, cos, pi, tau
+from math import pi, tau, sin, cos
 
 import bmesh
 from mathutils import Matrix
@@ -30,35 +30,34 @@ def create_prongs(self):
     # Prong
     # ---------------------------
 
-    prong_size = self.diameter / 2
+    prong_rad = self.diameter / 2
     taper = self.taper + 1
 
-    if self.bump_scale == 0.0:
-
-        v_cos = (
-            (0.0, 0.0,                 self.z_top),
-            (0.0, prong_size,          self.z_top),
-            (0.0, prong_size * taper, -self.z_btm),
-        )
-
-    else:
+    if self.bump_scale:
 
         curve_resolution = int(self.detalization / 4) + 1
         angle = (pi / 2) / (curve_resolution - 1)
 
         v_cos = []
         v_co_app = v_cos.append
+        x = 0.0
 
         for i in range(curve_resolution):
-            x = 0.0
-            y = sin(i * angle) * prong_size
-            z = cos(i * angle) * prong_size * self.bump_scale + self.z_top
+            y = sin(i * angle) * prong_rad
+            z = cos(i * angle) * prong_rad * self.bump_scale + self.z_top
             v_co_app((x, y, z))
 
-        v_co_app((x, prong_size * taper, -self.z_btm))
+        v_co_app((x, prong_rad * taper, -self.z_btm))
+
+    else:
+
+        v_cos = (
+            (0.0, 0.0,                self.z_top),
+            (0.0, prong_rad,          self.z_top),
+            (0.0, prong_rad * taper, -self.z_btm),
+        )
 
     bm = bmesh.new()
-
     v_profile = [bm.verts.new(v) for v in v_cos]
 
     for i in range(len(v_profile) - 1):
@@ -73,17 +72,17 @@ def create_prongs(self):
     # Transforms
     # ---------------------------
 
-    if self.alignment != 0.0:
+    pos_offset = (self.gem_l / 2 + prong_rad) - (self.diameter * (self.intersection / 100))
+    spin_steps = self.number - 1
+
+    if self.alignment:
         bmesh.ops.rotate(bm, verts=bm.verts, cent=(0.0, 0.0, 0.0), matrix=Matrix.Rotation(-self.alignment, 4, "X"))
 
-    offset = (self.gem_l / 2) + prong_size - (prong_size * 2) * (self.intersection / 100)
-    spin_dupl = self.number - 1
-    spin_angle = tau - tau / self.number
+    bmesh.ops.translate(bm, verts=bm.verts, vec=(0.0, pos_offset, 0.0))
 
-    bmesh.ops.translate(bm, verts=bm.verts, vec=(0.0, offset, 0.0))
-
-    if spin_dupl > 0:
-        bmesh.ops.spin(bm, geom=bm.faces, angle=spin_angle, steps=spin_dupl, axis=(0.0, 0.0, 1.0), cent=(0.0, 0.0, 0.0), use_duplicate=True)
+    if spin_steps:
+        spin_angle = tau - tau / self.number
+        bmesh.ops.spin(bm, geom=bm.faces, angle=spin_angle, steps=spin_steps, axis=(0.0, 0.0, 1.0), cent=(0.0, 0.0, 0.0), use_duplicate=True)
 
     bmesh.ops.rotate(bm, verts=bm.verts, cent=(0.0, 0.0, 0.0), matrix=Matrix.Rotation(-self.position, 4, "Z"))
 
