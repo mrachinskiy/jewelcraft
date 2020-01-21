ec90dd8db27aad1b4f0df7c47478818825f6b8d6
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-01-21 10:06:28 +0400

    Update copyright year

--- ops_weighting/weighting_set_ops.py
+++ ops_weighting/weighting_set_ops.py
@@ -1,7 +1,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  JewelCraft jewelry design toolkit for Blender.
-#  Copyright (C) 2015-2019  Mikhail Rachinskiy
+#  Copyright (C) 2015-2020  Mikhail Rachinskiy
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
