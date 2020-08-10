65ae9834ce2ddf0ba66ef29a5428de02228cd7a9
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-08-10 23:11:49 +0400

    Replace spline.calc_length() with mesh implementation
    spline.calc_length() does not take modifiers into account.

--- lib/mesh.py
+++ lib/mesh.py
@@ -97,15 +97,48 @@ def est_volume(obs):
 
 
 def est_curve_length(ob):
-    curve = ob.data.copy()
-    curve.transform(ob.matrix_world)
+    # NOTE spline.calc_length() is useless, more info T65152
+
+    # Reset curve
+    # ---------------------------
+
+    settings = {
+        "bevel_object": None,
+        "bevel_depth": 0.0,
+        "extrude": 0.0,
+    }
+
+    for k, v in settings.items():
+        x = getattr(ob.data, k)
+        setattr(ob.data, k, v)
+        settings[k] = x
+
+    # Calculate length
+    # ---------------------------
+
+    depsgraph = bpy.context.evaluated_depsgraph_get()
+
+    ob_eval = ob.evaluated_get(depsgraph)
+    me = ob_eval.to_mesh()
+    me.transform(ob.matrix_world)
+
+    bm = bmesh.new()
+    bm.from_mesh(me)
+
+    ob_eval.to_mesh_clear()
 
     length = 0.0
 
-    for spline in curve.splines:
-        length += spline.calc_length()
+    for edge in bm.edges:
+        length += edge.calc_length()
 
-    bpy.data.curves.remove(curve)
+    bm.free()
+
+    # Restore curve
+    # ---------------------------
+
+    for k, v in settings.items():
+        setattr(ob.data, k, v)
 
     return length
 
