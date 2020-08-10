8c1ba94575a8bff17692b11df88e534f575f9743
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-08-10 06:03:33 +0400

    ui_lib: add type annotations

--- lib/ui_lib.py
+++ lib/ui_lib.py
@@ -19,24 +19,29 @@
 # ##### END GPL LICENSE BLOCK #####
 
 
-def popup_report(self, context, text="", title="", icon="INFO"):
+from __future__ import annotations
+from typing import List
+
+
+def popup_report(self, context, msg: str = "", title: str = "", icon: str = "INFO") -> None:
 
     def draw(self, context):
-        self.layout.label(text=text)
+        self.layout.label(text=msg)
 
-    self.report({"INFO"}, f"{title}: {text}")
+    self.report({"INFO"}, f"{title}: {msg}")
     context.window_manager.popup_menu(draw, title=title, icon=icon)
 
 
-def popup_report_batch(self, context, data=None, title="", icon="INFO"):
+def popup_report_batch(self, context, msgs: List[str] = None, title: str = "", icon: str = "INFO") -> None:
 
     def draw(self, context):
         layout = self.layout
-        for text in data:
+        for text in msgs:
             layout.label(text=text)
 
-    self.report({"INFO"}, title + ":\n")
-    for text in data:
+    self.report({"OPERATOR"}, f"{title}:")
+
+    for text in msgs:
         self.report({"INFO"}, text)
 
     context.window_manager.popup_menu(draw, title=title, icon=icon)
