912277798c9d9bd1d609e2ce5fe0ff73fdc1ca28
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-08-11 14:52:13 +0400

    Use spline.calc_length() for curves without mods

--- lib/mesh.py
+++ lib/mesh.py
@@ -97,51 +97,59 @@ def est_volume(obs):
 
 
 def est_curve_length(ob):
-    # NOTE spline.calc_length() is useless, more info T65152
-
-    # Reset curve
-    # ---------------------------
-
-    settings = {
-        "bevel_object": None,
-        "bevel_depth": 0.0,
-        "extrude": 0.0,
-    }
-
-    for k, v in settings.items():
-        x = getattr(ob.data, k)
-        setattr(ob.data, k, v)
-        settings[k] = x
-
-    # Calculate length
-    # ---------------------------
-
     if ob.modifiers:
+
+        # Reset curve
+        # ---------------------------
+
+        settings = {
+            "bevel_object": None,
+            "bevel_depth": 0.0,
+            "extrude": 0.0,
+        }
+
+        for k, v in settings.items():
+            x = getattr(ob.data, k)
+            setattr(ob.data, k, v)
+            settings[k] = x
+
+        # Calculate length
+        # ---------------------------
+
         depsgraph = bpy.context.evaluated_depsgraph_get()
         ob_eval = ob.evaluated_get(depsgraph)
+        me = ob_eval.to_mesh()
+        me.transform(ob.matrix_world)
+
+        bm = bmesh.new()
+        bm.from_mesh(me)
+
+        ob_eval.to_mesh_clear()
+
+        length = 0.0
+
+        for edge in bm.edges:
+            length += edge.calc_length()
+
+        bm.free()
+
+        # Restore curve
+        # ---------------------------
+
+        for k, v in settings.items():
+            setattr(ob.data, k, v)
+
     else:
-        ob_eval = ob
 
-    me = ob_eval.to_mesh()
-    me.transform(ob.matrix_world)
+        curve = ob.data.copy()
+        curve.transform(ob.matrix_world)
 
-    bm = bmesh.new()
-    bm.from_mesh(me)
+        length = 0.0
 
-    ob_eval.to_mesh_clear()
+        for spline in curve.splines:
+            length += spline.calc_length()
 
-    length = 0.0
-
-    for edge in bm.edges:
-        length += edge.calc_length()
-
-    bm.free()
-
-    # Restore curve
-    # ---------------------------
-
-    for k, v in settings.items():
-        setattr(ob.data, k, v)
+        bpy.data.curves.remove(curve)
 
     return length
 
