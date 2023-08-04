c8ef676f459e036602f73d58b9519c094b4f542f
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2023-08-04 11:29:16 +0400

    Update copyright year

--- lib/view3d_lib/__init__.py
+++ lib/view3d_lib/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright 2015-2022 Mikhail Rachinskiy
+# Copyright 2015-2023 Mikhail Rachinskiy
 
 from .view3d_overlay import *
 from .view3d_text import *
