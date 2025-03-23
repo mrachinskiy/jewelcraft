de55a534b4ef26822aa77ff0d440dc7d8a96ef00
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2025-03-23 20:53:33 +0400

    Fix asset rename icon in Blender 4.3 and above

--- ops_asset/asset_ops.py
+++ ops_asset/asset_ops.py
@@ -316,6 +316,9 @@ class WM_OT_asset_menu(Operator):
 
     filepath: StringProperty(options={"SKIP_SAVE", "HIDDEN"})
 
+    # VER >= 4.3
+    icon_rename = "GREASEPENCIL" if bpy.app.version >= (4, 3, 0) else "OUTLINER_DATA_GP_LAYER"
+
     def execute(self, context):
         return {"FINISHED"}
 
@@ -329,7 +332,7 @@ class WM_OT_asset_menu(Operator):
         layout.emboss = "PULLDOWN_MENU"
 
         row = layout.row()
-        row.operator("wm.jewelcraft_asset_rename", text="", icon="OUTLINER_DATA_GP_LAYER").filepath = self.filepath
+        row.operator("wm.jewelcraft_asset_rename", text="", icon=self.icon_rename).filepath = self.filepath
         row.operator("wm.jewelcraft_asset_preview_replace", text="", icon="IMAGE_DATA").filepath = self.filepath
         row.operator("wm.jewelcraft_asset_replace", text="", icon="FILE_TICK").filepath = self.filepath
         row.operator("wm.jewelcraft_asset_remove", text="", icon="TRASH").filepath = self.filepath
