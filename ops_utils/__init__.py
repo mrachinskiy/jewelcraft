324d53d3879cd1e6e766535819f9085ce727324b
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2025-03-30 04:13:24 +0400

    Update copyright year

--- ops_utils/__init__.py
+++ ops_utils/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2015-2024 Mikhail Rachinskiy
+# SPDX-FileCopyrightText: 2015-2025 Mikhail Rachinskiy
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .list_ops import *
