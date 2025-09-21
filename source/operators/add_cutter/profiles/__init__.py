76bf049deb78ece7dddaf0e94c28607bc23b97c4
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2025-09-21 22:01:00 +0400

    Cleanup: use abstract class for return

--- source/operators/add_cutter/profiles/__init__.py
+++ source/operators/add_cutter/profiles/__init__.py
@@ -1,11 +1,26 @@
 # SPDX-FileCopyrightText: 2015-2025 Mikhail Rachinskiy
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from bmesh.types import BMesh, BMVert
+from mathutils import Vector
+
 from ....lib import gemlib
 from . import fantasy, rectangle, round, triangle
 
 
-def get(op):
+class Section:
+
+    def add(self, bm: BMesh, size: Vector, offset: tuple[float, float, float] = None) -> tuple[list[BMVert], list[BMVert]]:
+        ...
+
+    def add_z_fmt(self, bm: BMesh, size: Vector) -> tuple[list[BMVert], list[BMVert]]:
+        ...
+
+    def add_seat_rect(self, bm: BMesh, girdle_verts: list[BMVert], Girdle: Vector, Hole: Vector) -> None:
+        ...
+
+
+def get(op) -> Section:
     match op.shape:
         case gemlib.SHAPE_ROUND:
             return round.Section(op)
